93ca5379ac011ce8a6131f6d111410c9d7101804
shadrach256 <shadrachssozi18@gmail.com>
2025-04-08 16:40:06 +0300

    Add files via upload

---
+++ answers.sql
@@ -0,0 +1,5 @@
+-- Question 1: Create a new database called salesDB
+CREATE DATABASE salesDB;
+
+-- Question 2: Drop (delete) the database called demo
+DROP DATABASE IF EXISTS demo;  -- 'IF EXISTS' prevents errors if the database doesn't exist
